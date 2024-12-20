bab1a9418ab3ebb20a7566c54618546be3750117
Lv Yonghuan <1914128278@qq.com>
2024-12-20 23:08:21 +0800

    Test init core from confg

--- ubikHost/core_test.cs
+++ ubikHost/core_test.cs
@@ -1,6 +1,14 @@
+using NUnit.Framework;
+
 namespace ubikHost;
 
+[TestFixture]
 public class CoreTest
 {
-    
+    private const string ConfigPath = "../../../example-config.json";
+    [Test]
+    public  void TestInitCore()
+    {
+        var c = new Core(ConfigPath);
+    }
 }
