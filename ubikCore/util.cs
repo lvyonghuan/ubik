5b026a64c25d0ce4d740be40212dcaee3884830d
Lv Yonghuan <1914128278@qq.com>
2024-12-20 16:15:38 +0800

    finish test util package

--- ubikCore/util.cs
+++ ubikCore/util.cs
@@ -6,19 +6,19 @@ public class UbikUtil
 {
     public class UbikLogger
     {
-        private readonly int _level;// 日志等级
-        private readonly bool _isSave;// 是否保存日志
-        private readonly string _logSavePath = "./log"; // 日志保存路径
+        private static  int _level;// 日志等级
+        private static  bool _isSave;// 是否保存日志
+        private static  string _logSavePath = "./log"; // 日志保存路径
 
-        private readonly object _fileLock = new object(); // 文件互斥访问
+        private static readonly object FileLock = new object(); // 文件互斥访问
 
         // 日志等级
-        private const int OffLevel = 0;
-        private const int FatalLevel = 1;
-        private const int ErrorLevel = 2;
-        private const int WarnLevel = 3;
-        private const int InfoLevel = 4; // 默认级别
-        private const int DebugLevel = 5;
+        public const int OffLevel = 0;
+        public const int FatalLevel = 1;
+        public const int ErrorLevel = 2;
+        public const int WarnLevel = 3;
+        public const int InfoLevel = 4; // 默认级别
+        public const int DebugLevel = 5;
 
         // 日志颜色
         private const string Reset = "\033[0m";
@@ -32,87 +32,72 @@ public class UbikUtil
         {
             _level = level;
             _isSave = isSave;
-            if (_isSave)
-            {
-                var currentTime = DateTime.Now.ToString("_yyyy-MM-dd HH-mm-ss");
-                 _logSavePath=savePath  + currentTime + ".log";
-            }
+            if (!_isSave) return;
+            var currentTime = DateTime.Now.ToString("_yyyy-MM-dd HH-mm-ss");
+            _logSavePath = savePath + currentTime + ".log";
         }
 
         // Debug 打印debug级别日志
         public void Debug(string v)
         {
-            if (_level >= DebugLevel)
-            {
-                var logString = "Debug: " + v;
-                Console.WriteLine(Green + logString + Reset);
-                SaveLogToFile(logString);
-            }
+            if (_level < DebugLevel) return;
+            var logString = "Debug: " + v;
+            Console.WriteLine(Green + logString + Reset);
+            SaveLogToFile(logString);
         }
 
         // Info 打印info级别日志
         public void Info(string v)
         {
-            if (_level >= InfoLevel)
-            {
-                var logString = v;
-                Console.WriteLine(logString);
-                SaveLogToFile(logString);
-            }
+            if (_level < InfoLevel) return;
+            Console.WriteLine(v);
+            SaveLogToFile(v);
         }
 
         // Warn 打印warn级别日志
         public void Warn(string v)
         {
-            if (_level >= WarnLevel)
-            {
-                var logString = "Warn: " + v;
-                Console.WriteLine(Yellow + logString + Reset);
-                SaveLogToFile(logString);
-            }
+            if (_level < WarnLevel) return;
+            var logString = "Warn: " + v;
+            Console.WriteLine(Yellow + logString + Reset);
+            SaveLogToFile(logString);
         }
 
         // Error 打印error级别日志
         public void Error(Exception v)
         {
-            if (_level >= ErrorLevel)
-            {
-                var logString = "Error: " + v.Message;
-                Console.WriteLine(Orange + logString + Reset);
-                SaveLogToFile(logString);
-            }
+            if (_level < ErrorLevel) return;
+            var logString = "Error: " + v.Message;
+            Console.WriteLine(Orange + logString + Reset);
+            SaveLogToFile(logString);
         }
 
         // Fatal 打印fatal级别日志
         public void Fatal(Exception v)
         {
-            if (_level >= FatalLevel)
-            {
-                var logString = "Fatal: " + v.Message;
-                Console.WriteLine(Red + logString + Reset);
-                SaveLogToFile(logString);
-            }
+            if (_level < FatalLevel) return;
+            var logString = "Fatal: " + v.Message;
+            Console.WriteLine(Red + logString + Reset);
+            SaveLogToFile(logString);
         }
 
         // SaveLogToFile 保存日志到文件
         private void SaveLogToFile(string v)
         {
-            if (_isSave)
+            if (!_isSave) return;
+            lock (FileLock)
             {
-                lock (_fileLock)
+                try
                 {
-                    try
+                    using (var file = new StreamWriter(_logSavePath, true))
                     {
-                        using (var file = new StreamWriter(_logSavePath, true))
-                        {
-                            file.WriteLine(DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") + ":" + v);
-                        }
+                        file.WriteLine(DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") + ":" + v);
                     }
-                    catch (Exception ex)
-                    {
-                        // 唯二不调用error级别却打印error日志的地方
-                        Warn("Write log to file failed: " + ex.Message);
-                    }
+                }
+                catch (Exception ex)
+                {
+                    // 唯二不调用error级别却打印error日志的地方
+                    Warn("Write log to file failed: " + ex.Message);
                 }
             }
         }
---
+++ ubikCore/util_test.cs
@@ -0,0 +1,46 @@
+namespace ubikCore;
+
+using NUnit.Framework;
+using System;
+
+[TestFixture]
+public class UbikLoggerTests
+{
+    [Test]
+    public void TestLogPrint()
+    {
+        var logger = new UbikUtil.UbikLogger(UbikUtil.UbikLogger.DebugLevel, false, "");
+        logger.Debug("test debug");
+        logger.Info("test info");
+        logger.Warn("test warn");
+        logger.Error(new Exception("test error"));
+        logger.Fatal(new Exception("test fatal"));
+
+        TestContext.Out.WriteLine("Completed TestLogPrint");
+    }
+
+    [Test]
+    public void TestLogSave()
+    {
+        var logger = new UbikUtil.UbikLogger(5, true, "./");
+        logger.Debug("test debug");
+        logger.Info("test info");
+        logger.Warn("test warn");
+        logger.Error(new Exception("test error"));
+        logger.Fatal(new Exception("test fatal"));
+
+        TestContext.Out.WriteLine("Completed TestLogSave");
+    }
+}
+
+[TestFixture]
+public class UbikExceptionTests
+{
+    [Test]
+    public void TestNewUbikException()
+    {
+        var ex = new UbikUtil.UbikException("an error occurred");
+        TestContext.WriteLine(ex.ToString());
+        TestContext.WriteLine(ex.ErrorMessage);
+    }
+}
